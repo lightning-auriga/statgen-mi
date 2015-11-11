dcb2599076650591fb5630f23f5aee26a220e8f3
cpalmer718 <palmer.cam@gmail.com>
2015-11-11 17:20:10 -0500

    Removing libtool, adding subdirectory config.h

---
+++ fileinterface/fileinterface/config.h
@@ -0,0 +1,127 @@
+#ifndef _FILEINTERFACE_CONFIG_H
+#define _FILEINTERFACE_CONFIG_H 1
+ 
+/* fileinterface/config.h. Generated automatically at end of configure. */
+/* temp_config.h.  Generated from temp_config.h.in by configure.  */
+/* temp_config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if your C++ compiler doesn't accept -c and -o together. */
+/* #undef CXX_NO_MINUS_C_MINUS_O */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#ifndef FILEINTERFACE_HAVE_DLFCN_H
+#define FILEINTERFACE_HAVE_DLFCN_H 1
+#endif
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#ifndef FILEINTERFACE_HAVE_INTTYPES_H
+#define FILEINTERFACE_HAVE_INTTYPES_H 1
+#endif
+
+/* Define to 1 if you have `bz2' library (-lbz2) */
+#ifndef FILEINTERFACE_HAVE_LIBBZ2
+#define FILEINTERFACE_HAVE_LIBBZ2 1
+#endif
+
+/* Define to 1 if you have the `m' library (-lm). */
+#ifndef FILEINTERFACE_HAVE_LIBM
+#define FILEINTERFACE_HAVE_LIBM 1
+#endif
+
+/* Define to 1 if you have `z' library (-lz) */
+#ifndef FILEINTERFACE_HAVE_LIBZ
+#define FILEINTERFACE_HAVE_LIBZ 1
+#endif
+
+/* Define to 1 if you have the <memory.h> header file. */
+#ifndef FILEINTERFACE_HAVE_MEMORY_H
+#define FILEINTERFACE_HAVE_MEMORY_H 1
+#endif
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#ifndef FILEINTERFACE_HAVE_STDINT_H
+#define FILEINTERFACE_HAVE_STDINT_H 1
+#endif
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#ifndef FILEINTERFACE_HAVE_STDLIB_H
+#define FILEINTERFACE_HAVE_STDLIB_H 1
+#endif
+
+/* Define to 1 if you have the <strings.h> header file. */
+#ifndef FILEINTERFACE_HAVE_STRINGS_H
+#define FILEINTERFACE_HAVE_STRINGS_H 1
+#endif
+
+/* Define to 1 if you have the <string.h> header file. */
+#ifndef FILEINTERFACE_HAVE_STRING_H
+#define FILEINTERFACE_HAVE_STRING_H 1
+#endif
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#ifndef FILEINTERFACE_HAVE_SYS_STAT_H
+#define FILEINTERFACE_HAVE_SYS_STAT_H 1
+#endif
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#ifndef FILEINTERFACE_HAVE_SYS_TYPES_H
+#define FILEINTERFACE_HAVE_SYS_TYPES_H 1
+#endif
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#ifndef FILEINTERFACE_HAVE_UNISTD_H
+#define FILEINTERFACE_HAVE_UNISTD_H 1
+#endif
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#ifndef FILEINTERFACE_LT_OBJDIR
+#define FILEINTERFACE_LT_OBJDIR ".libs/"
+#endif
+
+/* Name of package */
+#ifndef FILEINTERFACE_PACKAGE
+#define FILEINTERFACE_PACKAGE "fileinterface"
+#endif
+
+/* Define to the address where bug reports for this package should be sent. */
+#ifndef FILEINTERFACE_PACKAGE_BUGREPORT
+#define FILEINTERFACE_PACKAGE_BUGREPORT "cdp2130@columbia.edu"
+#endif
+
+/* Define to the full name of this package. */
+#ifndef FILEINTERFACE_PACKAGE_NAME
+#define FILEINTERFACE_PACKAGE_NAME "fileinterface"
+#endif
+
+/* Define to the full name and version of this package. */
+#ifndef FILEINTERFACE_PACKAGE_STRING
+#define FILEINTERFACE_PACKAGE_STRING "fileinterface 1.0"
+#endif
+
+/* Define to the one symbol short name of this package. */
+#ifndef FILEINTERFACE_PACKAGE_TARNAME
+#define FILEINTERFACE_PACKAGE_TARNAME "fileinterface"
+#endif
+
+/* Define to the home page for this package. */
+#ifndef FILEINTERFACE_PACKAGE_URL
+#define FILEINTERFACE_PACKAGE_URL ""
+#endif
+
+/* Define to the version of this package. */
+#ifndef FILEINTERFACE_PACKAGE_VERSION
+#define FILEINTERFACE_PACKAGE_VERSION "1.0"
+#endif
+
+/* Define to 1 if you have the ANSI C header files. */
+#ifndef FILEINTERFACE_STDC_HEADERS
+#define FILEINTERFACE_STDC_HEADERS 1
+#endif
+
+/* Version number of package */
+#ifndef FILEINTERFACE_VERSION
+#define FILEINTERFACE_VERSION "1.0"
+#endif
+ 
+/* once: _FILEINTERFACE_CONFIG_H */
+#endif
