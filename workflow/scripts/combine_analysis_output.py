ee12afdf50c71920c92e48ac94f7c4092720bae7
Lightning Auriga <lightning.auriga@gmail.com>
2022-08-14 18:55:08 -0400

    fix: adjust logic of mi combination due to NA entries in some runs

--- workflow/scripts/combine_analysis_output.py
+++ workflow/scripts/combine_analysis_output.py
@@ -9,19 +9,20 @@ def plink2_handler(lines, model_name):
     """
     varid = ""
     effect_allele = ""
+    sample_size = 0
     betas = []
     variances = []
     pattern = None
     if model_name == "linear":
         pattern = re.compile(
             "[^\t]+\t[^\t]+\t([^\t]+)\t[^\t]+\t[^\t]+\t([^\t]+)\t[^\t]+\t"
-            "[^\t]+\t[^\t]+\t[^\t]+\t([^\t]+)\t([^\t]+)\t"
+            "[^\t]+\t[^\t]+\t([^\t]+)\t([^\t]+)\t([^\t]+)\t"
         )
     elif model_name == "logistic":
         pattern = re.compile(
             "[^\t]+\t[^\t]+\t([^\t]+)\t[^\t]+\t[^\t]+\t([^\t]+)\t[^\t]+\t"
-            "[^\t]+\t[^\t]+\t[^\t]+\t([^\t]+)\t[^\t]+\t[^\t]+\t[^\t]+\t"
-            "[^\t]+\t([^\t]+)\t"
+            "[^\t]+\t[^\t]+\t[^\t]+\t([^\t]+)\t[^\t]+\t[^\t]+\t([^\t]+)\t"
+            "([^\t]+)\t([^\t]+)\t"
         )
     else:
         raise ValueError(
@@ -34,39 +35,52 @@ def plink2_handler(lines, model_name):
         if match:
             varid = match.group(1)
             effect_allele = match.group(2)
-            if model_name == "logistic":
-                betas.append(math.log(float(match.group(3))))
-            else:
-                betas.append(float(match.group(3)))
-            variances.append(pow(float(match.group(4)), 2))
+            if match.group(5) != "NA":
+                sample_size = int(match.group(3))
+                if model_name == "logistic":
+                    betas.append(math.log(float(match.group(4))))
+                else:
+                    betas.append(float(match.group(4)))
+                variances.append(pow(float(match.group(5)), 2))
         else:
             raise ValueError(
                 "plink2 regression result does not match expected format: {}".format(
                     line
                 )
             )
-    overall_beta = sum(betas) / len(betas)
-    within_variance = sum(variances) / len(variances)
-    between_variance = sum(
-        [pow(overall_beta - current_beta, 2) for current_beta in betas]
-    ) / len(betas)
-    total_variance = within_variance + (len(betas) + 1) / (
-        len(betas) * between_variance
-    )
-    dof = (len(betas) - 1) * pow(
-        1 + 1 / (len(betas) + 1) * within_variance / between_variance, 2
-    )
-    tstat = overall_beta / math.sqrt(total_variance)
-    return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-        varid,
-        effect_allele,
-        overall_beta,
-        within_variance,
-        between_variance,
-        total_variance,
-        dof,
-        tstat,
-    )
+    if len(betas) > 0:
+        overall_beta = sum(betas) / len(betas)
+        within_variance = sum(variances) / len(variances)
+        between_variance = sum(
+            [pow(overall_beta - current_beta, 2) for current_beta in betas]
+        ) / len(betas)
+        total_variance = (
+            within_variance + (len(betas) + 1) / len(betas) * between_variance
+        )
+        fraction_total_missing = (
+            between_variance * (len(betas) + 1 / len(betas))
+        ) / total_variance
+        dof_old = (len(betas) - 1) / pow(fraction_total_missing, 2)
+        dof_obs = (
+            (sample_size - 1)
+            / (sample_size + 1)
+            * (sample_size - 2)
+            * (1 - fraction_total_missing)
+        )
+        dof_adj = dof_old * dof_obs / (dof_old + dof_obs)
+        tstat = overall_beta / math.sqrt(total_variance)
+        return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+            varid,
+            effect_allele,
+            overall_beta,
+            within_variance,
+            between_variance,
+            total_variance,
+            dof_adj,
+            tstat,
+        )
+    else:
+        return "{}\t{}\tNA\tNA\tNA\tNA\tNA\n".format(varid, effect_allele)
 
 
 def run_combine_analysis_output(snakemake):
