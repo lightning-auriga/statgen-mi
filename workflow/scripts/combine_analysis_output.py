c08c1bb477163b094a1ac2ad8e7dcf6e6c061ae9
Lightning Auriga <lightning.auriga@gmail.com>
2022-08-14 20:49:29 -0400

    fix: refactor MI calculation and enforce convergence criteria for all tests

--- workflow/scripts/combine_analysis_output.py
+++ workflow/scripts/combine_analysis_output.py
@@ -3,6 +3,52 @@ import re
 from contextlib import ExitStack
 
 
+def format_mi_result(varid, effect_allele, sample_size, betas, variances, mi_run_count):
+    """
+    given parsed information from multiple association runs,
+    combine and return MI results
+    """
+    if len(betas) == mi_run_count:
+        overall_beta = sum(betas) / len(betas)
+        within_variance = sum(variances) / len(variances)
+        between_variance = sum(
+            [pow(overall_beta - current_beta, 2) for current_beta in betas]
+        ) / len(betas)
+        # set test statistic defaults assuming invariant draws, as will
+        # happen for perfect confidence genotypes
+        dof_adj = 1
+        tstat = overall_beta / math.sqrt(within_variance)
+        if not math.isclose(between_variance, 0):
+            total_variance = (
+                within_variance + (len(betas) + 1) / len(betas) * between_variance
+            )
+            fraction_total_missing = (
+                between_variance * (len(betas) + 1 / len(betas))
+            ) / total_variance
+            dof_old = (len(betas) - 1) / pow(fraction_total_missing, 2)
+            dof_obs = (
+                (sample_size - 1)
+                / (sample_size + 1)
+                * (sample_size - 2)
+                * (1 - fraction_total_missing)
+            )
+            dof_adj = dof_old * dof_obs / (dof_old + dof_obs)
+            tstat = overall_beta / math.sqrt(total_variance)
+        return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
+            varid,
+            effect_allele,
+            overall_beta,
+            within_variance,
+            between_variance,
+            total_variance,
+            dof_adj,
+            tstat,
+        )
+    # for the time being, enforce all simulations converge. if they don't,
+    # treat the result as not converged
+    return "{}\t{}\tNA\tNA\tNA\tNA\tNA\n".format(varid, effect_allele)
+
+
 def plink2_handler(lines, model_name):
     """
     handle MI combination for results from plink2 glm
@@ -48,39 +94,9 @@ def plink2_handler(lines, model_name):
                     line
                 )
             )
-    if len(betas) > 0:
-        overall_beta = sum(betas) / len(betas)
-        within_variance = sum(variances) / len(variances)
-        between_variance = sum(
-            [pow(overall_beta - current_beta, 2) for current_beta in betas]
-        ) / len(betas)
-        total_variance = (
-            within_variance + (len(betas) + 1) / len(betas) * between_variance
-        )
-        fraction_total_missing = (
-            between_variance * (len(betas) + 1 / len(betas))
-        ) / total_variance
-        dof_old = (len(betas) - 1) / pow(fraction_total_missing, 2)
-        dof_obs = (
-            (sample_size - 1)
-            / (sample_size + 1)
-            * (sample_size - 2)
-            * (1 - fraction_total_missing)
-        )
-        dof_adj = dof_old * dof_obs / (dof_old + dof_obs)
-        tstat = overall_beta / math.sqrt(total_variance)
-        return "{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-            varid,
-            effect_allele,
-            overall_beta,
-            within_variance,
-            between_variance,
-            total_variance,
-            dof_adj,
-            tstat,
-        )
-    else:
-        return "{}\t{}\tNA\tNA\tNA\tNA\tNA\n".format(varid, effect_allele)
+    return format_mi_result(
+        varid, effect_allele, sample_size, betas, variances, len(lines)
+    )
 
 
 def run_combine_analysis_output(snakemake):
